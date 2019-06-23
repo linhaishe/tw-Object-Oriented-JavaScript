669160f3e190fc32b9079b3565d6932cef2ae698
linhaishe <xusumu@gmail.com>
2019-06-23 11:28:30 +0800

    finished prac3

--- main/practice_3/worker.js
+++ main/practice_3/worker.js
@@ -1,1 +1,9 @@
+let Person = require("../../main/practice_3/person.js");
 
+class Worker extends Person{
+    introduce(){
+        return "I am a Worker. I have a job.";
+    }
+}
+
+module.exports = Worker
