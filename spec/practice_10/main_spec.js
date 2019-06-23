d4f7c0ecff6f5507fc213745d8830abacd1569e0
linhaishe <xusumu@gmail.com>
2019-06-23 23:43:37 +0800

    assignLeader error

--- spec/practice_10/main_spec.js
+++ spec/practice_10/main_spec.js
@@ -1,15 +1,24 @@
 "use strict";
-import _ from "lodash";
-import chai from "chai";
-import sinon from "sinon";
-import sinonChai from "sinon-chai";
-const expect = chai.expect;
-chai.use(sinonChai);
+// import _ from "lodash";
+// import chai from "chai";
+// import sinon from "sinon";
+// import sinonChai from "sinon-chai";
+// const expect = chai.expect;
+// chai.use(sinonChai);
 
-import Person from "../../main/practice_10/person.js";
-import Student from "../../main/practice_10/student.js";
-import Teacher from "../../main/practice_10/teacher.js";
-import Class from "../../main/practice_10/class.js";
+// import Person from "../../main/practice_10/person.js";
+// import Student from "../../main/practice_10/student.js";
+// import Teacher from "../../main/practice_10/teacher.js";
+// import Class from "../../main/practice_10/class.js";
+
+let expect = require("chai").expect;
+let sinonChai = require("sinon-chai");
+
+let Person = require("../../main/practice_10/person.js");
+let Student = require("../../main/practice_10/student.js");
+let Teacher = require("../../main/practice_10/teacher.js")
+let Class = require("../../main/practice_10/class.js")
+
 
 describe("Person", () => {
     it("should have field name and age", () => {
